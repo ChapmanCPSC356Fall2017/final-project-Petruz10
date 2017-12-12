b265bc103b9cc99fa8d6be11d351038673b5320f
Petruz <petrairisl36@gmail.com>
2017-12-11 20:30:05 -0800

    the photos are saving!

--- app/src/main/java/com/example/petruz/cameraapp/Activities/CameraActivity.java
+++ app/src/main/java/com/example/petruz/cameraapp/Activities/CameraActivity.java
@@ -2,7 +2,9 @@ package com.example.petruz.cameraapp.Activities;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.ImageFormat;
 import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
@@ -16,10 +18,14 @@ import android.hardware.camera2.TotalCaptureResult;
 import android.hardware.camera2.params.StreamConfigurationMap;
 import android.media.Image;
 import android.media.ImageReader;
+import android.net.Uri;
+import android.os.Debug;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -28,6 +34,7 @@ import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.petruz.cameraapp.R;
@@ -38,8 +45,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.ByteBuffer;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 public class CameraActivity extends AppCompatActivity {
@@ -52,10 +61,10 @@ public class CameraActivity extends AppCompatActivity {
     private Button getpictureBtn;
     private HandlerThread mBackgroundThread;
     private Handler mBackgroundHandler;
-    private File file;
 
 
     private final int MY_PERMISSIONS_REQUEST_CAMERA = 1;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
     private final String LOGTAG = "Camera Activity";
 
     @Override
@@ -63,140 +72,85 @@ public class CameraActivity extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        takePicture();
+    //    this.textureView = (TextureView) findViewById(R.id.tv_camera);
+    //    this.textureView.setSurfaceTextureListener(surfaceTextureListener);
 
-        this.textureView = (TextureView) findViewById(R.id.tv_camera);
-        this.textureView.setSurfaceTextureListener(surfaceTextureListener);
+    //    this.getpictureBtn = (Button) findViewById(R.id.takePictureBtn);
+    //    this.getpictureBtn.setOnClickListener(new View.OnClickListener() {
+    //        @Override
+    //        public void onClick(View view) {
 
-        this.getpictureBtn = (Button) findViewById(R.id.takePictureBtn);
-        this.getpictureBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                takePicture();
-            }
-        });
+      //      }
+      //  });
 
     }
 
-    protected void takePicture() {
+    String mCurrentPhotoPath;
+    private ImageView mImageView;
+
+    private File createImageFile() throws IOException
+    {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp;
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        Log.e(LOGTAG, "Image is" + image);
+
+
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    protected void takePicture()
+    {
         if(null == cameraDevice) {
             Log.e(LOGTAG, "cameraDevice is null");
-            return;
+            //return;
         }
-        CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-        try {
-            CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraDevice.getId());
-            Size[] jpegSizes = null;
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-            if (characteristics != null)
-            {
-                jpegSizes = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP).getOutputSizes(ImageFormat.JPEG);
-            }
-
-            int width = 640;
-            int height = 480;
-            if (jpegSizes != null && 0 < jpegSizes.length)
-            {
-                width = jpegSizes[0].getWidth();
-                height = jpegSizes[0].getHeight();
-            }
-
-            ImageReader reader = ImageReader.newInstance(width, height, ImageFormat.JPEG, 1);
-            List<Surface> outputSurfaces = new ArrayList<Surface>(2);
-            outputSurfaces.add(reader.getSurface());
-            outputSurfaces.add(new Surface(textureView.getSurfaceTexture()));
-
-            final CaptureRequest.Builder captureBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
-            captureBuilder.addTarget(reader.getSurface());
-            captureBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
-            // Orientation
-           // int rotation = getWindowManager().getDefaultDisplay().getRotation();
-            captureBuilder.set(CaptureRequest.JPEG_ORIENTATION, Surface.ROTATION_0);
-            final File file = new File(Environment.getExternalStorageDirectory()+"/pic.jpg");
-
-            ImageReader.OnImageAvailableListener readerListener = new ImageReader.OnImageAvailableListener()
-            {
-                @Override
-                public void onImageAvailable(ImageReader reader)
-                {
-                    Image image = null;
-                    try
-                    {
-                        image = reader.acquireLatestImage();
-                        ByteBuffer buffer = image.getPlanes()[0].getBuffer();
-                        byte[] bytes = new byte[buffer.capacity()];
-                        buffer.get(bytes);
-                        save(bytes);
-                    }
-                    catch (FileNotFoundException e)
-                    {
-                        e.printStackTrace();
-                    }
-                    catch (IOException e)
-                    {
-                        e.printStackTrace();
-                    }
-                    finally
-                    {
-                        if (image != null)
-                        {
-                            image.close();
-                        }
-                    }
-                }
-
-                private void save(byte[] bytes) throws IOException
-                {
-                    OutputStream output = null;
-                    try
-                    {
-                        output = new FileOutputStream(file);
-                        output.write(bytes);
-                    }
-                    finally
-                    {
-                        if (null != output)
-                        {
-                            output.close();
-                        }
-                    }
-                }
-            };
-            reader.setOnImageAvailableListener(readerListener, mBackgroundHandler);
-
-            final CameraCaptureSession.CaptureCallback captureListener = new CameraCaptureSession.CaptureCallback()
-            {
-                @Override
-                public void onCaptureCompleted(CameraCaptureSession session, CaptureRequest request, TotalCaptureResult result)
-                {
-                    super.onCaptureCompleted(session, request, result);
-                    Toast.makeText(CameraActivity.this, "Saved:" + file, Toast.LENGTH_SHORT).show();
-                    startCamera();
-                }
-            };
-
-            cameraDevice.createCaptureSession(outputSurfaces, new CameraCaptureSession.StateCallback()
-            {
-                @Override
-                public void onConfigured(CameraCaptureSession session) {
-                    try {
-                        session.capture(captureBuilder.build(), captureListener, mBackgroundHandler);
-                    } catch (CameraAccessException e) {
-                        e.printStackTrace();
-                    }
-                }
-                @Override
-                public void onConfigureFailed(CameraCaptureSession session) {
-                }
-            }, mBackgroundHandler);
-        }
-        catch (CameraAccessException e)
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null)
         {
-            e.printStackTrace();
+            // Create the File where the photo should go
+            File photoFile = null;
+            try
+            {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null)
+            {
+                final File file = new File(Environment.getExternalStorageDirectory()+"/pic.jpg");
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, file);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+
+        //openCamera();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            super.onActivityResult(requestCode, resultCode, data);
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            // imageBitmap = (Bitmap) data.getExtras().get("data");
+            mImageView.setImageBitmap(imageBitmap);
         }
     }
 
-    private TextureView.SurfaceTextureListener surfaceTextureListener = new TextureView.SurfaceTextureListener()
-    {
+    private TextureView.SurfaceTextureListener surfaceTextureListener = new TextureView.SurfaceTextureListener() {
         @Override
         public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
             openCamera();
@@ -216,6 +170,7 @@ public class CameraActivity extends AppCompatActivity {
         }
     };
 
+
     private boolean checkCameraHardware(Context context) {
         if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
             // this device has a camera
@@ -231,6 +186,7 @@ public class CameraActivity extends AppCompatActivity {
     private void openCamera()
     {
         CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
         try {
             String camerId = manager.getCameraIdList()[0];
             CameraCharacteristics characteristics = manager.getCameraCharacteristics(camerId);
@@ -239,18 +195,29 @@ public class CameraActivity extends AppCompatActivity {
 
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
             {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+
                 // https://developer.android.com/training/permissions/requesting.html
-                //checks if the user have allowed the app to use the camera if not, ask them
+                //checkes if the user have allowed the app to use the camera if not, ask them
                 if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                         Manifest.permission.CAMERA))
                 {
+
                     // Show an explanation to the user *asynchronously* -- don't block
                     // this thread waiting for the user's response! After the user
                     // sees the explanation, try again to request the permission.
+
                 }
                 else
                 {
                     // No explanation needed, we can request the permission.
+
                     ActivityCompat.requestPermissions(this,
                             new String[]{Manifest.permission.CAMERA},
                             MY_PERMISSIONS_REQUEST_CAMERA);
--- app/src/main/java/com/example/petruz/cameraapp/Fragments/StartupFragment.java
+++ app/src/main/java/com/example/petruz/cameraapp/Fragments/StartupFragment.java
@@ -10,10 +10,14 @@ import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.params.StreamConfigurationMap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
 import android.util.Log;
 import android.util.Size;
 import android.view.LayoutInflater;
@@ -26,6 +30,11 @@ import com.example.petruz.cameraapp.Activities.CameraActivity;
 import com.example.petruz.cameraapp.MainActivity;
 import com.example.petruz.cameraapp.R;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static android.content.ContentValues.TAG;
 
 /**
@@ -36,13 +45,27 @@ public class StartupFragment extends Fragment
 {
     private static String LOGTAG = "STARTUP_CAMERA";
 
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private String mCurrentPhotoPath;
+
  //   private TextureView textureView;
+    /*
+     * TODO:
+     * skapa en recycler view där bilderna visas i detta fragment
+     * on activity result, öppna en ny activity där man kan lägga till text osv osv
+     */
 
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
+        File storageDir = getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        for (File f : storageDir.listFiles())
+        {
+            Log.i(LOGTAG, f.getName());
+        }
     }
 
     @Nullable
@@ -56,9 +79,48 @@ public class StartupFragment extends Fragment
 
     public void showCamera()
     {
-        Intent myIntent = new Intent(this.getActivity(), CameraActivity.class);
-        this.getActivity().startActivity(myIntent);
+        takePicture();
+    }
 
+    protected void takePicture()
+    {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (takePictureIntent.resolveActivity(getContext().getPackageManager()) != null)
+        {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try
+            {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                Log.e(LOGTAG, "there was an error" + ex.getMessage());
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null)
+            {
+                Uri photoUri = FileProvider.getUriForFile(this.getContext(), "com.example.petruz.cameraapp.fileprovider", photoFile);
+
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException
+    {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp;
+        File storageDir = getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",         /* suffix */
+                storageDir             /* directory */
+        );
+
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 
 }
